719f75f2b91f18fe63c09f415d57982c320c54cb
adama-traore-github <wwwnostra7@gmail.com>
2024-07-17 21:37:42 +0200

    first commit

---
+++ Projet_Rails_movies_app/db/seeds.rb
@@ -0,0 +1,27 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+
+require 'faker'
+
+genres = ["action", "horreur", "comédie", "drame"]
+
+100.times do
+  Movie.create(
+    name: Faker::Movie.title,
+    year: rand(1900..2020),
+    genre: genres.sample,
+    synopsis: Faker::Lorem.sentence(word_count: 10),
+    director: Faker::Name.name,
+    mycine_rating: rand(0.0..5.0).round(1),
+    my_rating: nil,
+    already_seen: false
+  )
+end
